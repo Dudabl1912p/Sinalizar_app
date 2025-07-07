cf9d997af5bd3d625baa7cb5c98ed62b2d774267
Dudabl1912p <eduardaoliveira1912p@gmail.com>
2025-07-07 19:47:21 -0300

    PDF

--- resources/views/components/catalogo/filtros.blade.php
+++ resources/views/components/catalogo/filtros.blade.php
@@ -40,7 +40,7 @@ $agrupadoPorCategoria = Produto::all()->groupBy('categoria');
 
 <div class="mb-4">
     <h5 class="border-start border-warning ps-2 mb-2">Saiba mais</h5>
-    <a href="{{ url('/catalogo') }}" class="btn btn-dark w-100 mb-2" target="_blank">Catálogo.pdf</a>
+    <a href="{{ url('/catalogo_Sinalizar') }}" class="btn btn-dark w-100 mb-2" target="_blank">Catálogo.pdf</a>
 </div>
 
 <div class="mb-4">
