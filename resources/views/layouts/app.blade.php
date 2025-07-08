b49245a939ea329cef73f3180fa2f985618028b5
Dudabl1912p <eduardaoliveira1912p@gmail.com>
2025-07-08 17:16:12 -0300

    icone redes sociais

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,8 @@
     <!-- Bootstrap e ícones -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+
     
     @vite(['resources/css/estilo.css', 'resources/js/mascara-telefone.js'])
 
